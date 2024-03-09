808c84b734e45536d9cf9d8c31d814871148053c
dddhiraj2001 <dddhiraj2001@gmail.com>
2024-03-08 23:19:34 -0500

    final Commit

---
+++ src/Storage.js
@@ -0,0 +1,93 @@
+class Storage{
+    static getCalorieLimit(defaultLimit = 2000){
+        let calorieLimit;
+        if(localStorage.getItem('calorieLimit')===null){
+            calorieLimit = defaultLimit
+        }else{
+            calorieLimit = +localStorage.getItem('calorieLimit')
+        }
+
+        return calorieLimit
+    }
+
+    static setCalorieLimit(calorieLimit){
+        localStorage.setItem('calorieLimit',calorieLimit)
+    }
+
+    static getTotalCalories(){
+        let totalCalories;
+        if(localStorage.getItem('calorieTotal')===null){
+            totalCalories = 0
+        }else{
+            totalCalories = +localStorage.getItem('calorieTotal')
+        }
+        return totalCalories
+    }
+    static setTotalCalories(calories){
+        localStorage.setItem('calorieTotal',calories)
+    }
+
+    static getMeals(){
+        let meals;
+        if(localStorage.getItem('meals')===null){
+            meals = []
+        }else{
+            meals= JSON.parse(localStorage.getItem('meals'))
+        }
+        return meals
+
+    }
+
+    static saveMeals(meal){
+        const meals = Storage.getMeals();
+        meals.push(meal)
+        localStorage.setItem('meals',JSON.stringify(meals))
+
+    }
+    static removeMeal(id){
+        const meals = Storage.getMeals();
+        meals.forEach((meal,index)=>{
+            if(meal.id===id){
+                meals.splice(index,1)
+            }
+        })
+        localStorage.setItem('meals',JSON.stringify(meals))
+    }
+    static removeWorkout(id){
+        const workouts = Storage.getWorkouts();
+        workouts.forEach((meal,index)=>{
+            if(meal.id===id){
+                workouts.splice(index,1)
+            }
+        })
+        localStorage.setItem('workouts',JSON.stringify(workouts))
+    }
+
+    static getWorkouts(){
+        let workouts;
+        if(localStorage.getItem('workouts')===null){
+            workouts = []
+        }else{
+            workouts= JSON.parse(localStorage.getItem('workouts'))
+        }
+        return workouts
+    }
+
+    static saveWorkouts(workout){
+        const workouts = Storage.getWorkouts()
+        workouts.push(workout)
+        localStorage.setItem('workouts',JSON.stringify(workouts))
+        
+    }
+
+    static clearAll(){
+        localStorage.removeItem('calorieTotal')
+        localStorage.removeItem('meals')
+        localStorage.removeItem('wokrouts')
+        // localStorage.clear()
+    }
+    
+   
+}
+
+export default Storage;
